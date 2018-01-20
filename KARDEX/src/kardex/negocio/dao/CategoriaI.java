34758e12a74d0ad882fd97d56dc6663121c2513c
Mishelll <SONY@DESKTOP-EIOR6CT>
2018-01-20 13:21:19 -0500

    subiendo categria

--- KARDEX/src/kardex/negocio/dao/CategoriaI.java
+++ KARDEX/src/kardex/negocio/dao/CategoriaI.java
@@ -10,6 +10,6 @@ public interface CategoriaI {
     public int insertar (Categoria categoria) throws Exception;
     public int modificar (Categoria categoria) throws Exception; 
     public int eliminar (Categoria categoria) throws Exception;
-    public Categoria obtener (String cedula) throws Exception;
+    public Categoria obtener(int codCategoria) throws Exception;
     public ArrayList<Categoria> obtener() throws Exception;
 }
