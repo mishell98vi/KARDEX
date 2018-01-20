87deae2ff926a632e63e8b8c5f82eac095a871be
Mishelll <SONY@DESKTOP-EIOR6CT>
2018-01-20 10:57:33 -0500

    parametro

--- KARDEX/src/kardex/accesoadatos/Parametro.java
+++ KARDEX/src/kardex/accesoadatos/Parametro.java
@@ -4,11 +4,34 @@
  * and open the template in the editor.
  */
 package kardex.accesoadatos;
+import java.util.*;
+import java.sql.*;
 
-/**
- *
- * @author SONY
- */
 public class Parametro {
-    
+      private int posicion;
+    private Object valor;
+
+    public Parametro() {
+    }
+
+    public Parametro(int posicion, Object valor) {
+        this.posicion = posicion;
+        this.valor = valor;
+    }
+
+    public int getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(int posicion) {
+        this.posicion = posicion;
+    }
+
+    public Object getValor() {
+        return valor;
+    }
+
+    public void setValor(Object valor) {
+        this.valor = valor;
+    }
 }
