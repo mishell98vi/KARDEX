0be7a7e86c33160641db154d99a8236a6be2ba06
Mishelll <SONY@DESKTOP-EIOR6CT>
2018-01-20 15:19:42 -0500

    Test Cliente

---
+++ KARDEX/test/kardex/TestCliente.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kardex;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import kardex.accesoadatos.*;
+import kardex.negocio.dao.*;
+import kadex.negocio.entidades.*;
+import kadex.negocios.impl.*;
+import java.util.*;
+import java.sql.*;
+public class TestCliente {
+    
+    public TestCliente() {
+    }
+    @Test
+    public void testGeneral(){
+        ClienteI clDao=new ClienteImp();
+        ///////TEST INSERTAR
+//        int filas = 0;
+
+
+//
+//        Cliente cln = new Cliente("0603437047", "Daniel", "Bastian", "10 de agosto", "0973140672", "ao@gmail.com", new java.util.Date());
+//        try {
+//            filas = clDao.insertar(cln);
+//            System.out.println("filas Inseertadas: " + filas + "\n");
+//        } catch (Exception e) {
+//        }
+//        assertEquals(filas > 0, true);
+        ////TEST OBTENER CODIGO
+//        Cliente cli = new Cliente();
+//
+//        try {
+//            cli = clDao.obtener("0603437047");
+//            System.out.println(cli.getCedula()+" "+cli.getNombre() +" "+ cli.getApellido()+" "+cli.getDireccion()+" "+cli.getTelefono()+" "+cli.getEmail()+" "+cli.getFechaNac());
+//        } catch (Exception e) {
+//        }
+
+        //////// TEST LISTADO 
+        ArrayList<Cliente> clientess = new ArrayList<>();
+        try {
+            clientess = clDao.obtener();
+            for (Cliente clientes : clientess) {
+                System.out.println("CEDULA      " + "  NOMBRE "+ "   APELLIDO"+"DIRECCION"+"TELEFONO"+"EMAIL"+"FECHA NACIEMIENTO");
+                System.out.println(clientes.getCedula() + "    " + clientes.getNombre() + "    " + clientes.getApellido() + "      " + clientes.getDireccion() + " " + clientes.getTelefono() + "" + clientes.getEmail() + " "+ clientes.getFechaNac());
+            }
+
+        } catch (Exception e) {
+        }
+        assertTrue(clientess.size() > 0);
+    }
+    
+}
