4d5bcb729a27280ee04e370fe10cfc7a01b2bd0f
Jorge <JhonPC@DESKTOP-T9J7GH5>
2018-01-20 10:09:47 -0500

    Entidad Detalle_Venta

--- KARDEX/src/kadex/negocio/entidades/Detalle_Venta.java
+++ KARDEX/src/kadex/negocio/entidades/Detalle_Venta.java
@@ -1,14 +1,62 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package kadex.negocio.entidades;
 
-/**
- *
- * @author SONY
- */
 public class Detalle_Venta {
+    private int codigoDVenta;
+    private Producto producto;
+    private Factura_Venta fVenta;
+    private int cantidad;
+    private double pTotal;
+
+    public Detalle_Venta() {
+    }
+
+    public Detalle_Venta(int codigoDVenta, Producto producto, Factura_Venta fVenta, int cantidad, double pTotal) {
+        this.codigoDVenta = codigoDVenta;
+        this.producto = producto;
+        this.fVenta = fVenta;
+        this.cantidad = cantidad;
+        this.pTotal = pTotal;
+    }
+
+    public int getCodigoDVenta() {
+        return codigoDVenta;
+    }
+
+    public void setCodigoDVenta(int codigoDVenta) {
+        this.codigoDVenta = codigoDVenta;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Factura_Venta getfVenta() {
+        return fVenta;
+    }
+
+    public void setfVenta(Factura_Venta fVenta) {
+        this.fVenta = fVenta;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getpTotal() {
+        return pTotal;
+    }
+
+    public void setpTotal(double pTotal) {
+        this.pTotal = pTotal;
+    }
     
 }
