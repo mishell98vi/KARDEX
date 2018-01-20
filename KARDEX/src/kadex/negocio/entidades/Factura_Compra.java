d56b8e3bee9970706a18829852da650ee44cad0b
Mishelll <SONY@DESKTOP-EIOR6CT>
2018-01-20 09:52:01 -0500

    subiendo entidad de factura_compra

--- KARDEX/src/kadex/negocio/entidades/Factura_Compra.java
+++ KARDEX/src/kadex/negocio/entidades/Factura_Compra.java
@@ -5,10 +5,44 @@
  */
 package kadex.negocio.entidades;
 
-/**
- *
- * @author SONY
- */
+import java.util.*;
 public class Factura_Compra {
+ 
+    private int codigoFCompra;
+    private Date Fecha;
+    private Proveedor proveedor;
+
+    public Factura_Compra() {
+    }
+
+    public Factura_Compra(int codigoFCompra, Date Fecha, Proveedor proveedor) {
+        this.codigoFCompra = codigoFCompra;
+        this.Fecha = Fecha;
+        this.proveedor = proveedor;
+    }
+
+    public int getCodigoFCompra() {
+        return codigoFCompra;
+    }
+
+    public void setCodigoFCompra(int codigoFCompra) {
+        this.codigoFCompra = codigoFCompra;
+    }
+
+    public Date getFecha() {
+        return Fecha;
+    }
+
+    public void setFecha(Date Fecha) {
+        this.Fecha = Fecha;
+    }
+
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
     
 }
