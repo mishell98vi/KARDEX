ef31725f4021d553d9f176ea7570ae917cd450e1
Mishell <>
2018-01-26 23:17:34 -0500

    Correccion de errores

--- KARDEX/src/kardex/accesoadatos/Conexion.java
+++ KARDEX/src/kardex/accesoadatos/Conexion.java
@@ -9,81 +9,80 @@ import java.sql.*;
 import java.util.*;
 import java.util.Date;
 public class Conexion {
-     Scanner entrada = new Scanner(System.in);
     String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     String url = "jdbc:sqlserver://localhost:1433;databaseName=Proyecto";
-    String usuario = "sebas";
-    String conraseña = "sebas2";
-    Connection conexion = null;
+    String user = "sebas";
+    String pass = "sebas2";
+    Connection con = null;
 
     public void conectar() {
 
         try {
             Class.forName(driver);
-            conexion = DriverManager.getConnection(url, usuario, conraseña);
+            con = DriverManager.getConnection(url, user, pass);
             System.out.println("Conexion Establecida!!!");
         } catch (ClassNotFoundException e) {
             System.out.println("Error al cargar Driver: " + e.getMessage());
         } catch (SQLException e) {
             System.out.println("Error de SQL: " + e.getMessage());
         } catch (Exception e) {
-            System.out.println("Error en conexion: " + e.getMessage());
+            System.out.println("Error en con: " + e.getMessage());
         }
     }
 
-    public ResultSet ejecutarQuery(String comandoSQL, List<Parametro> listaParametros) {
-        ResultSet resultado = null;
+    public ResultSet ejecutarQuery(String comandoSQL, List<Parametro> lstParam) {
+        ResultSet rest = null;
         try {
-            PreparedStatement estado = conexion.prepareStatement(comandoSQL);
-            if (listaParametros != null) {
-                for (Parametro valorP : listaParametros) {
+            PreparedStatement state = con.prepareStatement(comandoSQL);
+            if (lstParam != null) {
+                for (Parametro valorP : lstParam) {
                     
                     if (valorP.getValor() instanceof java.util.Date) {
-                        estado.setObject(valorP.getPosicion(), new java.sql.Date(((java.util.Date) valorP.getValor()).getTime()));
+                        state.setObject(valorP.getPosicion(), new java.sql.Date(((java.util.Date) valorP.getValor()).getTime()));
                     } else {
-                        estado.setObject(valorP.getPosicion(), valorP.getValor());
+                        state.setObject(valorP.getPosicion(), valorP.getValor());
                     }
 
                 }
             }
-            resultado = estado.executeQuery();
+            rest = state.executeQuery();
         }catch (Exception e) {
             System.out.println("Error en Ejecucion SQL: " + e.getMessage());
         }
-        return resultado;
+        return rest;
     }
 
     public int ejecutarComando(String sql, ArrayList<Parametro> ValoresParametro) {
-        int nFilasAfectadas=0;
-        ResultSet resultado = null;
+        int nFilasAfect=0;
+        ResultSet rest = null;
         try {
-            PreparedStatement estado = conexion.prepareStatement(sql);
+            PreparedStatement state = con.prepareStatement(sql);
             if (ValoresParametro != null) {
                 for (Parametro valorP : ValoresParametro) {
                     if (valorP.getValor() instanceof java.util.Date) {
-                        estado.setObject(valorP.getPosicion(), new java.sql.Date(((java.util.Date) valorP.getValor()).getTime()));
+                        state.setObject(valorP.getPosicion(), new java.sql.Date(((java.util.Date) valorP.getValor()).getTime()));
                     } else {
-                        estado.setObject(valorP.getPosicion(), valorP.getValor());
+                        state.setObject(valorP.getPosicion(), valorP.getValor());
                     }
                 }
             }
-            nFilasAfectadas=estado.executeUpdate();
+            nFilasAfect=state.executeUpdate();
         } catch (Exception e) {
             System.out.println("Error en Ejecucion SQL: " + e.getMessage());
         }
-        return nFilasAfectadas;
+        return nFilasAfect;
     }
 
     public void desconectar() {
 
         try {
-            if (conexion != null) {
-                if (!conexion.isClosed()) {
-                    conexion.close();
+            if (con != null) {
+                if (!con.isClosed()) {
+                    con.close();
                 }
             }
         } catch (Exception e) {
-            System.out.println("Error de desconexion: " + e.getMessage());
+            System.out.println("Error de descon: " + e.getMessage());
         }
 
     }
--- KARDEX/src/kardex/accesoadatos/Parametro.java
+++ KARDEX/src/kardex/accesoadatos/Parametro.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kardex.accesoadatos;
+
 import java.util.*;
 import java.sql.*;
 
 public class Parametro {
-      private int posicion;
+
+    private int posicion;
     private Object valor;
 
     public Parametro() {
