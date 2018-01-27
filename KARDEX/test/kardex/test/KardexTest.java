8a051ff61213b0dd52772897ae7749d5abe2c407
Mishell <>
2018-01-26 23:19:26 -0500

    Testeos

---
+++ KARDEX/test/kardex/test/KardexTest.java
@@ -0,0 +1,17 @@
+
+package kardex.test;
+import kardex.negocio.dao.*;
+import kardex.accesoadatos.*;
+import kardex.negocio.entidades.*;
+import kardex.negocio.impl.*;
+import java.util.*;
+import java.sql.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class KardexTest {
+    
+    public KardexTest() {
+    }
+    
+}
