9a1c547059640e197bf71d8c628b1b79f208ba90
mishell viteri <>
2018-02-05 12:36:14 -0500

    Vistas

--- KARDEX/src/kardex/vistas/Form_Categoria.java
+++ KARDEX/src/kardex/vistas/Form_Categoria.java
@@ -1,14 +1,103 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package kardex.vistas;
 
-/**
- *
- * @author JhonPC
- */
-public class Form_Categoria {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
+import javafx.stage.Stage;
+
+public class Form_Categoria extends Application {
+    private Label titulo;
+    private Label txtCodigo;
+    private Label txtNombre;
+    private Label txtDescripcion;
+    private TextField codigo;
+    private TextField nombre;
+    private TextField descripcion;
+    private Image icono;
+    private ImageView visor;
+    private Button btnIngresar;
+    private Button btnLimpiar;
+    private Button btnCancelar;
+    private VBox pnlICat1;
+    private VBox pnlICat2;
+    private HBox pnlcat;
+    private VBox pnlICat;
+    private HBox pnlSup;
+    private VBox pnlInt;
+    private HBox pnlBotones;
+    private VBox pntPricipal;
+    @Override
+    public void start(Stage primaryStage) {
+        //superior
+        titulo=new Label("\" Categorias \"");
+        titulo.setFont(Font.font("News701 BT", 30));
+        txtCodigo=new Label("Codigo: ");
+        txtCodigo.setFont(Font.font("News701 BT", 20));
+        txtNombre=new Label("Nombre: ");
+        txtNombre.setFont(Font.font("News701 BT", 20));
+        pnlICat1=new VBox(10);
+        pnlICat1.getChildren().addAll(txtCodigo,txtNombre);
+        pnlICat1.setAlignment(Pos.CENTER_RIGHT);
+        pnlICat1.setPadding(new Insets(10));
+        codigo=new TextField("");
+        nombre=new TextField("");
+        pnlICat2=new VBox(10);
+        pnlICat2.getChildren().addAll(codigo,nombre);
+        pnlICat2.setAlignment(Pos.CENTER);
+        pnlICat2.setPadding(new Insets(10));
+        pnlcat=new HBox(10);
+        pnlcat.getChildren().addAll(pnlICat1,pnlICat2);
+        pnlcat.setPadding(new Insets(10));
+        pnlICat=new VBox(10);
+        pnlICat.getChildren().addAll(titulo,pnlcat);
+        pnlICat.setAlignment(Pos.CENTER);
+        pnlSup=new HBox(10);
+        icono=new Image("file:src\\kardex\\multimedia\\images\\iconoCategoria.png");
+        visor=new ImageView(icono);
+        visor.setFitHeight(75);
+        visor.setFitWidth(75);
+        pnlSup.getChildren().addAll(pnlICat,visor);
+        pnlSup.setAlignment(Pos.CENTER);
+        txtDescripcion=new Label("Descripcion: ");
+        txtDescripcion.setFont(Font.font("News701 BT", 25));
+        descripcion=new TextField("");
+        descripcion.setMaxSize(350, 100);
+        descripcion.setMinSize(350, 100);
+        pnlInt=new VBox(10);
+        pnlInt.getChildren().addAll(txtDescripcion,descripcion);
+        pnlInt.setAlignment(Pos.CENTER);
+        btnIngresar=new Button("Ingresar");
+        btnIngresar.setFont(Font.font("News701 BT", 20));
+        btnLimpiar=new Button("Limpiar");
+        btnLimpiar.setFont(Font.font("News701 BT", 20));
+        btnCancelar=new Button("Cancelar");
+        btnCancelar.setFont(Font.font("News701 BT", 20));
+        pnlBotones=new HBox(25);
+        pnlBotones.getChildren().addAll(btnIngresar,btnLimpiar,btnCancelar);
+        pnlBotones.setAlignment(Pos.CENTER);
+        pntPricipal=new VBox(10);
+        pntPricipal.getChildren().addAll(pnlSup,pnlInt,pnlBotones);
+        pntPricipal.setPadding(new Insets(10));
+        
+        Scene scene = new Scene(pntPricipal, 420, 370);
+        
+        primaryStage.setTitle("Categoria");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
     
 }
--- KARDEX/src/kardex/vistas/Form_DetalleCompra.java
+++ KARDEX/src/kardex/vistas/Form_DetalleCompra.java
@@ -1,14 +1,108 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kardex.vistas;
 
-/**
- *
- * @author JhonPC
- */
-public class Form_DetalleCompra {
-    
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import java.util.*;
+import javafx.scene.text.*;
+import javafx.scene.paint.*;
+import java.lang.reflect.Field;
+import java.text.DateFormat;
+import java.text.MessageFormat;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import kardex.negocio.dao.*;
+import kardex.negocio.entidades.*;
+import kardex.negocio.impl.*;
+
+public class Form_DetalleCompra extends Application {
+
+    private Label txtcod;
+    private Label txtProd;
+    private Label txtPrecio;
+    private Label precio;
+    private Label txtCant;
+    private Label txtFactComp;
+    private Label txtTotal;
+    private Label total;
+    private TextField cod;
+    private TextField cant;
+    private ComboBox<Producto> lstProd;
+    private ComboBox<Factura_Compra> lstFactura;
+    private Image icono;
+    private ImageView visor;
+    private Button btnIngresar;
+    private Button btnLimpiar;
+    private Button btnCancelar;
+    private HBox pnlcod;
+    private HBox pnlProd;
+    private HBox pnlprecio;
+    private HBox pnlcant;
+    private VBox pnlfactura;
+    private HBox pnlBotones;
+    private VBox pnlSup;
+    private VBox pntPrincipal;
+
+    @Override
+    public void start(Stage primaryStage) {
+        txtcod=new Label("Codigo: ");
+        txtProd=new Label("Producto: ");
+        txtPrecio=new Label("Precio: ");
+        precio=new Label("");
+        txtCant=new Label("Cantidad: ");
+        txtFactComp=new Label("Factura: ");
+        txtTotal=new Label("Total: ");
+        total=new Label("");
+        cod=new TextField("");
+        cant=new TextField("");
+        lstProd=new ComboBox<>();
+        lstFactura=new ComboBox<>();
+        pnlcod=new HBox(5);
+        pnlcod.getChildren().addAll(txtcod, cod);
+        pnlcod.setPadding(new Insets(5));
+        pnlcod.setAlignment(Pos.CENTER);
+        pnlProd=new HBox(5);
+        pnlProd.getChildren().addAll(txtProd, lstProd, txtPrecio, precio);
+        pnlProd.setPadding(new Insets(5));
+        pnlProd.setAlignment(Pos.CENTER);
+        pnlcant=new HBox(5);
+        pnlcant.getChildren().addAll(txtCant,cant,txtTotal,total);
+        pnlcant.setPadding(new Insets(5));
+        pnlcant.setAlignment(Pos.CENTER);
+        pnlfactura=new VBox(5);
+        pnlfactura.getChildren().addAll(txtFactComp, lstFactura);
+        pnlfactura.setPadding(new Insets(5));
+        pnlfactura.setAlignment(Pos.CENTER);
+        btnIngresar=new Button("Ingresar");
+        btnLimpiar=new Button("Limpiar");
+        btnCancelar=new Button("Cancelar");
+        pnlBotones=new HBox(25);
+        pnlBotones.getChildren().addAll(btnIngresar,btnLimpiar,btnCancelar);
+        pnlBotones.setPadding(new Insets(5));
+        pnlBotones.setAlignment(Pos.CENTER);
+        pntPrincipal=new VBox(5);
+        pntPrincipal.getChildren().addAll(pnlcod,pnlProd,pnlcant,pnlfactura,pnlBotones);
+        pntPrincipal.setPadding(new Insets(10));
+        pntPrincipal.setAlignment(Pos.CENTER);
+        
+        
+        Scene scene = new Scene(pntPrincipal, 300, 250);
+
+        primaryStage.setTitle("Detalle Compra");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
