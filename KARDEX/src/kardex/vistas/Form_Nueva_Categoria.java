a2e3f6d89ab205597ae4078471d9a22a6982ba17
mishell viteri <>
2018-02-19 22:36:58 -0500

    categoria

--- KARDEX/src/kardex/vistas/Form_Nueva_Categoria.java
+++ KARDEX/src/kardex/vistas/Form_Nueva_Categoria.java
@@ -102,7 +102,7 @@ public class Form_Nueva_Categoria{
         pnlBotones.getChildren().addAll(btnIngresar, btnLimpiar);
         pnlBotones.setAlignment(Pos.CENTER);
         pnlFinal = new VBox(10);
-        Image fondoFinal=new Image("file:src\\kardex\\multimedia\\images\\fondo.jpg");
+        Image fondoFinal=new Image("file:src\\kardex\\multimedia\\images\\FONDOO.jpg");
         BackgroundImage fondo=new BackgroundImage(fondoFinal, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
         pnlFinal.setBackground(new Background(fondo));
         pnlFinal.setStyle("-fx-padding: 10; -fx-border-color: orange ; -fx-border-width: 2px");
